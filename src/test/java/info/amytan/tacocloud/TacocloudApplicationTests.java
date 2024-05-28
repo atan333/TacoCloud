4534b6a9269cc4e56d384db63461423e5c329b3d
atan333 <amytan@terpmail.umd.edu>
2024-05-28 15:42:52 -0400

    Home controller and test for home controller

---
+++ src/test/java/info/amytan/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package info.amytan.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
