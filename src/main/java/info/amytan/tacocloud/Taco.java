dca16278b3b517c6dcd23a875e00cb7af47941b6
atan333 <amytan@terpmail.umd.edu>
2024-05-29 13:40:00 -0400

    Taco class

--- src/main/java/info/amytan/tacocloud/Taco.java
+++ src/main/java/info/amytan/tacocloud/Taco.java
@@ -1,4 +1,11 @@
 package info.amytan.tacocloud;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
 }
