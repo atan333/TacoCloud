60e8af932dc4c30d329080053d3b117bc3e81ad2
atan333 <amytan@terpmail.umd.edu>
2024-06-05 13:03:10 -0400

    Design a taco view

--- src/main/java/info/amytan/tacocloud/DesignTacoController.java
+++ src/main/java/info/amytan/tacocloud/DesignTacoController.java
@@ -3,10 +3,7 @@ package info.amytan.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +29,7 @@ public class DesignTacoController {
                 new Ingredient("CHED", "Cheddar", Type.CHEESE),
                 new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE)
+                new Ingredient("MOZZ", "Mozzarella", Type.CHEESE)
         );
 
         Type[] types = Ingredient.Type.values();
@@ -53,7 +50,15 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm() {
-        return "design";
+        return "design-a-taco";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Proccessing taco: {}", taco);
+
+        return "redirect:orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
